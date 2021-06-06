c492a76269e735ed90ad4dc2030e22c27aad42e9
babita899 <babitasr8899@gmail.com>
2021-06-06 15:26:20 +0530

    fixng error

--- src/util.js
+++ src/util.js
@@ -1,2 +1,2 @@
-export const API_END_POINT_URL = 'https://bb-product--app.herokuapp.com/';//https://bb-product--app.herokuapp.com/
+export const API_END_POINT_URL = 'http://localhost:3001/';//https://bb-product--app.herokuapp.com/
 export const LOCAL_END_POINT_URL = 'http://localhost:3001/';//
